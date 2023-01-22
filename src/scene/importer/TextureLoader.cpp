ed6419b10d70ff35263f0215ba6dc7281691b005
Michael Babayan <michaelbabayan33@gmail.com>
2023-01-22 00:05:18 -0800

    mipmapping and anistrophic filtering

--- src/scene/importer/TextureLoader.cpp
+++ src/scene/importer/TextureLoader.cpp
@@ -18,6 +18,12 @@ Texture Scene::Importer::LoadTexture(Image image)
 	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+	float value, max_anisotropy = 4.0f;
+	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &value);
+	value = (value > max_anisotropy) ? max_anisotropy : value;
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, value);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.GetWidth(), image.GetHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.GetPixels());
 	glGenerateMipmap(GL_TEXTURE_2D);
